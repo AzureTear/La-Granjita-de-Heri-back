a6115ef80ff56b9381032b060fb64ed807236a94
AzureTear <ba.maiolatesi@duocuc.cl>
2025-11-12 23:17:12 -0300

    Initial commit

---
+++ src/test/java/com/example/La_Granjita_de_Heri/LaGranjitaDeHeriApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.La_Granjita_de_Heri;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LaGranjitaDeHeriApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
