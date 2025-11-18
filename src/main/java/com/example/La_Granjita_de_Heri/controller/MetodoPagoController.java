004650456c51c5bfda54c14530f5588495edd11b
niloheri123-ux <niloheri123@gmail.com>
2025-11-18 13:42:42 -0300

    con

---
+++ src/main/java/com/example/La_Granjita_de_Heri/controller/MetodoPagoController.java
@@ -0,0 +1,42 @@
+package com.example.La_Granjita_de_Heri.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.La_Granjita_de_Heri.model.MetodoEnvio;
+import com.example.La_Granjita_de_Heri.model.MetodoPago;
+import com.example.La_Granjita_de_Heri.service.MetodoPagoService;
+
+@RestController
+@RequestMapping("/api/metodosPago")
+public class MetodoPagoController {
+
+    @Autowired
+    private MetodoPagoService metodoPagoService;
+
+    @GetMapping
+    public ResponseEntity<List<MetodoPago>> getallMetodoEnvio() {
+        List<MetodoPago> metodoPagos = metodoPagoService.findAll();
+        if (metodoPagos.isEmpty()) {
+            return ResponseEntity.noContent().build();
+        }
+        return ResponseEntity.ok(metodoPagos);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<MetodoPago> getMetodoPagoById(@PathVariable Integer id) {
+        MetodoPago metodoPago = metodoPagoService.findById(id);
+        if (metodoPago == null) {
+            return ResponseEntity.notFound().build();
+        }
+        return ResponseEntity.ok(metodoPago);
+    }
+
+    
+}
