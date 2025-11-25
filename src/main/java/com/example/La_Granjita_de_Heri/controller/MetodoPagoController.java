52e93a6a0b60e48521b981cb2f85361be2ac6d6c
AzureTear <ba.maiolatesi@duocuc.cl>
2025-11-25 19:52:33 -0300

    Update MetodoPagoController.java

--- src/main/java/com/example/La_Granjita_de_Heri/controller/MetodoPagoController.java
+++ src/main/java/com/example/La_Granjita_de_Heri/controller/MetodoPagoController.java
@@ -6,6 +6,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -20,7 +22,7 @@ public class MetodoPagoController {
     private MetodoPagoService metodoPagoService;
 
     @GetMapping
-    public ResponseEntity<List<MetodoPago>> getallMetodoEnvio() {
+    public ResponseEntity<List<MetodoPago>> getallMetodoPago() {
         List<MetodoPago> metodoPagos = metodoPagoService.findAll();
         if (metodoPagos.isEmpty()) {
             return ResponseEntity.noContent().build();
@@ -37,5 +39,11 @@ public class MetodoPagoController {
         return ResponseEntity.ok(metodoPago);
     }
 
+    @PostMapping
+    public ResponseEntity<MetodoPago> createMetodoPago(@RequestBody MetodoPago metodoPago) {
+        metodoPago.setId(null);
+        MetodoPago metodoPagoNew = metodoPagoService.save(metodoPago);
+        return ResponseEntity.status(201).body(metodoPagoNew);
+    }
     
 }
